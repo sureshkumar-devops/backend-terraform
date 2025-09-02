3140c0632f594ba0a71bf4eb58305b9d901e0840
suresh <suresh@w3logics.com>
2025-09-02 16:40:19 +0000

    modified code

--- app.py
+++ app.py
@@ -5,10 +5,17 @@ import os
 
 app = Flask(__name__)
 
-DB_HOST = os.getenv("DB_HOST", "mysql")
-DB_USER = os.getenv("DB_USER", "admin")
-DB_PASS = os.getenv("DB_PASS", "password123")
-DB_NAME = os.getenv("DB_NAME", "coupondb")
+# DB_HOST = os.getenv("DB_HOST", "mysql")
+# DB_USER = os.getenv("DB_USER", "admin")
+# DB_PASS = os.getenv("DB_PASS", "Admin#8055")
+# DB_NAME = os.getenv("DB_NAME", "coupondb")
+
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("DB_NAME")
+
+
 
 @app.route("/api")
 def api():
